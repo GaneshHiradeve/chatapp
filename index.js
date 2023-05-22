50150cbfda7bd711fa74cdfe30067f057f1d4887
GaneshHiradeve <ganeshhiradeve2@gmail.com>
2023-05-22 23:40:38 +0530

    chat app

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const { config } = require('process');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const config=require('dotenv');
+const path = require('path');
+
+config({
+   path:".env"
+})
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
